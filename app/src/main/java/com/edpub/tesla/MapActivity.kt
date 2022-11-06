e0e659eb1654b596c5d57cf1f6ff00468b0bd604
xpandeyed <lalbiharipandeyg@gmail.com>
2022-11-06 14:26:20 +0530

    Zoom in and zoom out functionality added.

--- app/src/main/java/com/edpub/tesla/MapActivity.kt
+++ app/src/main/java/com/edpub/tesla/MapActivity.kt
@@ -2,10 +2,20 @@ package com.edpub.tesla
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.edpub.tesla.databinding.ActivityMapBinding
 
 class MapActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMapBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val mapFragment = binding.fcvMapContainer
+
+
     }
 }
--- app/src/main/java/com/edpub/tesla/MapsFragment.kt
+++ app/src/main/java/com/edpub/tesla/MapsFragment.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.edpub.tesla.databinding.FragmentMapsBinding
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -17,6 +18,8 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsFragment : Fragment() {
 
     private lateinit var map: GoogleMap
+
+    private lateinit var binding: FragmentMapsBinding
 
     private val callback = OnMapReadyCallback { googleMap ->
 
@@ -41,12 +44,33 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+
+        binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+
+        initView()
+
+    }
+
+    private fun initView(){
+        binding.cvZoomIn.setOnClickListener {
+            zoomIn()
+        }
+        binding.cvZoomOut.setOnClickListener {
+            zoomOut()
+        }
+    }
+
+    fun zoomIn(){
+        map.animateCamera(CameraUpdateFactory.zoomIn())
+    }
+    fun zoomOut(){
+        map.animateCamera(CameraUpdateFactory.zoomOut())
     }
 }
