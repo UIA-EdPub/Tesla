fb4fec1708dc504e404bc88793b1acdec1b0af94
xpandeyed <lalbiharipandeyg@gmail.com>
2022-11-06 19:41:19 +0530

    current location accessible now.

--- app/src/main/java/com/edpub/tesla/MapActivity.kt
+++ app/src/main/java/com/edpub/tesla/MapActivity.kt
@@ -1,7 +1,10 @@
 package com.edpub.tesla
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.edpub.tesla.databinding.ActivityMapBinding
 
 class MapActivity : AppCompatActivity() {
@@ -13,9 +16,5 @@ class MapActivity : AppCompatActivity() {
         binding = ActivityMapBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-
-        val mapFragment = binding.fcvMapContainer
-
-
     }
 }
--- app/src/main/java/com/edpub/tesla/MapsFragment.kt
+++ app/src/main/java/com/edpub/tesla/MapsFragment.kt
@@ -1,23 +1,53 @@
 package com.edpub.tesla
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
+import android.content.pm.PermissionInfo
+import android.location.Location
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.drawToBitmap
 import com.edpub.tesla.databinding.FragmentMapsBinding
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.security.Permissions
+
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import java.security.Permission
+
 
 class MapsFragment : Fragment() {
 
+    private val LOCATION_REQUEST_CODE = 1
+    private val DEFAULT_ZOOM = 18
+
     private lateinit var map: GoogleMap
+    private lateinit var cameraPosition: CameraPosition
+    private var locationPermissionGranted = false
+    private var lastKnownLocation: Location? = null
+    private val defaultLocation = LatLng(28.56172208815701, 77.28194072328036)
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     private lateinit var binding: FragmentMapsBinding
 
@@ -44,16 +74,16 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
         binding = FragmentMapsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        val mapFragment = childFragmentManager.findFragmentById(R.id.f_map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
-
+        checkPermission()
         initView()
 
     }
@@ -65,6 +95,14 @@ class MapsFragment : Fragment() {
         binding.cvZoomOut.setOnClickListener {
             zoomOut()
         }
+        binding.bTakeSs.setOnClickListener {
+            binding.llMap.drawToBitmap()
+        }
+
+        binding.cvLocateMe.setOnClickListener {
+            Log.i("fuckloction", "cv click")
+            getDeviceLocation()
+        }
     }
 
     fun zoomIn(){
@@ -73,4 +111,46 @@ class MapsFragment : Fragment() {
     fun zoomOut(){
         map.animateCamera(CameraUpdateFactory.zoomOut())
     }
+
+    private fun checkPermission(){
+        if(ActivityCompat.checkSelfPermission(requireContext(), ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            locationPermissionGranted = true
+        }else{
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+        }
+    }
+
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        Log.i("fucklocation", "cv click")
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            map?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+                        }
+                    } else {
+
+                        Log.d("fucklocation", "Current location is null. Using defaults.")
+                        Log.e("fucklocation", "Exception: %s", task.exception)
+                        map?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, DEFAULT_ZOOM.toFloat()))
+//                        map?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("fucklocation", e.message, e)
+        }
+    }
+
 }
