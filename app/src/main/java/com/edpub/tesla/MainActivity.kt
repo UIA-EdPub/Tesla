fa9c4ea7f6be06520ffdac5a578622c0d6f9b92f
xpandeyed <lalbiharipandeyg@gmail.com>
2022-11-06 12:40:31 +0530

    Google maps integrated.

--- app/src/main/java/com/edpub/tesla/MainActivity.kt
+++ app/src/main/java/com/edpub/tesla/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.edpub.tesla
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.edpub.tesla.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.bMap.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
